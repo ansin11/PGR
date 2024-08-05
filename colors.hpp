97c1f3a09ad70de351afa4288b44a3491aeafdd1
ansin11 <67983777+ansin11@users.noreply.github.com>
2024-08-05 22:37:07 +0200

    Replaced defines.hpp with *.inc files

--- colors.hpp
+++ colors.hpp
@@ -1,5 +1,13 @@
-#define PGR_COL_HIGHLIGHT "profileNamespace getVariable ['gui_bcg_rgb_r', 0.13]", "profileNamespace getVariable ['gui_bcg_rgb_g', 0.54]", "profileNamespace getVariable ['gui_bcg_rgb_b', 0.21]", "profileNamespace getVariable ['gui_bcg_rgb_a', 0.8]"
+#define PGR_COL_HIGHLIGHT \
+	"profileNamespace getVariable ['GUI_BCG_RGB_R', 0.13]", \
+	"profileNamespace getVariable ['GUI_BCG_RGB_G', 0.54]", \
+	"profileNamespace getVariable ['GUI_BCG_RGB_B', 0.21]", \
+	"profileNamespace getVariable ['GUI_BCG_RGB_A', 0.8]"
 
-#define PGR_COL_TEXT_TITLE "profileNamespace getVariable ['gui_titletext_rgb_r', 1]", "profileNamespace getVariable ['gui_titletext_rgb_g', 1]", "profileNamespace getVariable ['gui_titletext_rgb_b', 1]", "profileNamespace getVariable ['gui_titletext_rgb_a', 1]"
+#define PGR_COL_TEXT_TITLE \
+	"profileNamespace getVariable ['GUI_TITLETEXT_RGB_R', 1]", \
+	"profileNamespace getVariable ['GUI_TITLETEXT_RGB_G', 1]", \
+	"profileNamespace getVariable ['GUI_TITLETEXT_RGB_B', 1]", \
+	"profileNamespace getVariable ['GUI_TITLETEXT_RGB_A', 0.6]"
 
 #define PGR_COL_BACKGROUND 0, 0, 0, 0.6
--- dialog.hpp
+++ dialog.hpp
@@ -1,7 +1,8 @@
+#include "\a3\ui_f\hpp\defineResincl.inc"
+#include "\a3\ui_f\hpp\defineCommon.inc"
+
 #include "idcs.hpp"
 #include "colors.hpp"
 
-#include "dialog\defines.hpp"
 #include "dialog\imports.hpp"
-
 #include "dialog\PGR_Dialog.hpp"
